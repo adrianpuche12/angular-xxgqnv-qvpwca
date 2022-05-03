e68d1659abd57ebd48a6806046ae285fecccb268
adrianpuche12 <jorgeadrian_pucheta@hotmail.com>
2022-05-03 19:35:06 -0300

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,25 +10,31 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens',
+    description: 'An large phone with one of the best screens',
+    picture:
+      'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/400x400/public/media/image/2021/11/cardimage1636800119320-2531747.jpg?itok=zQMfTeGT',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras',
+    picture:
+      'https://images.yaoota.com/2MI7JE85ftMBVlrzykk-KnqOTDI=/trim/yaootaweb-production-sa/media/crawledproductimages/ba3836e9e2c5a1594c51ddd93f7fa6c739eb7840.jpg',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
     description: 'No tiene descripcion',
+    picture: 'https://i.blogs.es/166974/iphone-13-verde-iphone-13-pro-max-verde-alpino-diseno-color-applesfera-009/450_1000.jpeg'
   },
   {
     id: 3,
     name: 'Phone XpL',
     price: 200,
     description: 'No tiene descripcion',
+    picture: 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/09/21/16322254483747.png'
   },
 ];
 
